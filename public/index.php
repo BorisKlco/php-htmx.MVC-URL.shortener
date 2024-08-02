cf655718817f793e59ecb02abc2cd1104ae07be0
BorisKlco <klco@pm.me>
2024-08-02 20:19:33 +0000

    creating different router

--- public/index.php
+++ public/index.php
@@ -4,10 +4,35 @@ namespace App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-define('VIEW_PATH', __DIR__ . '/../views/');
+// define('VIEW_PATH', __DIR__ . '/../views/');
 
-$router = new Router();
+// $router = new Router();
 
-$router->get('/', [Controller\Home::class, 'main']);
+// $router->get('/', [Controller\Home::class, 'main']);
 
-echo $router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+// echo $router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+session_start();
+
+class Request
+{
+    public function __construct(
+        public readonly array $get,
+        public readonly array $post,
+        public readonly array $cookies,
+        public readonly array $files,
+        public readonly array $server
+
+    ) {
+    }
+
+    public static function test(): static
+    {
+        return new static($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+    }
+}
+
+$req = Request::test();
+
+
+echo '<pre>';
+print_r($req);
--- views/home.php
+++ views/home.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "home.php from views folder";
+
+$string = "This is an    example string";
+/* Use tab and newline as tokenizing characters as well  */
+$tok = strtok($string, " ");
+
+while ($tok !== false) {
+    echo "$tok<br />";
+    $tok = strtok(".");
+}
