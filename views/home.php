1ebdf7c75862ef029e4837c3b2f1d41a2897db28
BorisKlco <klco@pm.me>
2024-08-08 14:59:53 +0000

    link input

--- views/home.php
+++ views/home.php
@@ -19,13 +19,30 @@
         </nav>
         <button class="login"><span>Sign in</span></button>
     </div>
+    <h1 class="h1-info">More than a link shortener...</h1>
     <div class="hero">
 
-        <h1>Shorten links and generate QR codes.</h1>
 
         <div class="hero-main">
             <div></div>
-            <div class="hero-input">123</div>
+            <div class="hero-input">
+                <p class="input-info">Use URL shortener or QR generator and keep track of your connections...</p>
+                <form action="">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="link-icon"></i>
+                        </span>
+                        <input name="link" placeholder="Link it & ship it!" class="form-control" type="url">
+                    </div>
+                </form>
+                <div class="hero-div-submit">
+                    <span class="checkbox-span">
+                        <input type="checkbox" checked>
+                        <p class="checkbox-info">Insert fake views</p>
+                    </span>
+                    <button>Send</button>
+                </div>
+            </div>
         </div>
     </div>
 
